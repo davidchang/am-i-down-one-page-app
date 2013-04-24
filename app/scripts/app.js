6678a2b3f8db725eb65c90dfae21e7d07c022e3e
David Chang <dchang.me@gmail.com>
2013-04-24 12:15:48 -0700

    working with localStorage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('amIDownOnePageApp', [])
+angular.module('amIDownOnePageApp', ['LocalStorageModule'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('amIDownOnePageApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', ['$scope', 'localStorageService', function ($scope, ls) {
+
+      $scope.text = '';
+
+    try {
+        $scope.times = JSON.parse(ls.get('times')) || [];
+    } catch(err) {
+        ls.clearAll(); $scope.times = [];
+    }
+    $scope.save = function() {
+        $scope.times.push({text: $scope.text, time: new Date()});
+        ls.add('times', JSON.stringify($scope.times));
+        $scope.text = '';
+    }
   }]);
