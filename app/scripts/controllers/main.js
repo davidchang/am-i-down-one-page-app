f41503e1c34111faf0cb7f9ddef61d15337b2b37
David Chang <dchang.me@gmail.com>
2013-04-24 12:52:12 -0700

    some changes towards the real app, untested

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,14 @@
 angular.module('amIDownOnePageApp')
   .controller('MainCtrl', ['$scope', 'localStorageService', function ($scope, ls) {
 
+    if(ls.get('startTime')) {
+      $scope.startTime = ls.get('startTime');
+    }
+    else {
+        $scope.startTime = new Date().valueOf();
+        ls.add('startTime', new Date().valueOf());
+    }
+
       $scope.text = '';
 
     try {
@@ -10,9 +18,24 @@ angular.module('amIDownOnePageApp')
     } catch(err) {
         ls.clearAll(); $scope.times = [];
     }
-    $scope.save = function() {
-        $scope.times.push({text: $scope.text, time: new Date()});
+
+    var moodChange = function(isUp) {
+        //todo: delete
+        if($scope.text == 'clear') {
+            ls.clearAll();
+            return;
+        }
+        $scope.times.push({text: $scope.text, time: new Date(), up: isUp});
         ls.add('times', JSON.stringify($scope.times));
         $scope.text = '';
     }
+
+    $scope.moodDown = function() {
+        moodChange(false);
+    }
+    $scope.moodUp = function() {
+        moodChange(true);
+    }
+
+    $scope.currentlyDown = false;
   }]);
