c2a02d9d0475bd18b3e5af03d00502b88fdda2ea
David Chang <dchang.me@gmail.com>
2013-04-24 08:41:36 -0700

    initial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('amIDownOnePageApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
