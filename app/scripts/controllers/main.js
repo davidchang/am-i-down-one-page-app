bed66d8598743f18d9cd930788a6994cd2441858
David Chang <dchang.me@gmail.com>
2013-04-24 21:46:07 -0700

    uptime percentage working now, just no heatmap

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -39,9 +39,39 @@ angular.module('amIDownOnePageApp')
         moodChange(true);
     }
 
+    function calculate(times) {
+        if(!times || !times.length)
+            return 100;
+
+        var now = new Date().valueOf();
+        var totalMs = now - $scope.startTime;
+        var downTime = 0, lookingForDown = true, lastTime = null;
+        for(var i = 0, len = times.length; i < len; ++i) {
+            if(lookingForDown && !times[i].up) {
+                lastTime = times[i].time;
+                lookingForDown = false;
+            }
+            else {
+                downTime += (times[i].time - lastTime);
+                lookingForDown = true;
+            }
+        }
+        if(!lookingForDown)
+            downTime += (now - times[times.length - 1].time);
+
+        var percentage = (totalMs - downTime) * 100 / totalMs;
+        console.log(percentage);
+        return percentage;
+    }
+
     try {
         $scope.currentlyDown = $scope.times && !$scope.times[$scope.times.length - 1].up;
     } catch(err) { $scope.currentlyDown = false; }
+
+    $scope.uptime = calculate($scope.times);
+    setInterval(function() {
+        $scope.uptime = calculate($scope.times);
+    }, 5000);
 
     $scope.isUp = function(x) { return x ? 'up' : 'down'; }
   }]);
